e86fef612881cfff032c16d5f315b0a8d7e60fe5
Patryk Nusbaum <patryk@gociety.com>
2016-03-20 18:51:18 +0100

    moved the camera closer to the model

--- src/pl/pateman/skeletal/Main.java
+++ src/pl/pateman/skeletal/Main.java
@@ -123,10 +123,11 @@ public class Main {
             this.meshEntity.setMesh(mesh);
             this.meshEntity.setShaderProgram(this.meshProgram);
             this.meshEntity.buildMesh();
+            this.meshEntity.rotate(0.0f, (float) Math.toRadians(180.0f), 0.0f);
 
             //  Setup the camera.
             this.camera = new CameraEntity();
-            this.camera.translate(0.0f, 0.2f, -0.75f);
+            this.camera.translate(0.0f, 0.2f, -0.55f);
             this.camera.getCameraProjection().setViewport(WINDOW_WIDTH, WINDOW_HEIGHT);
             this.camera.updateProjectionMatrix();
         } catch (Exception e) {
